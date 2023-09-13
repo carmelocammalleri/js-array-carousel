5e811f239d957a7771b41693c08f1c8345894497
Melozzo <carmecamma1995@gmail.com>
2023-09-13 16:04:56 +0200

    [edit]: cicle for

--- js/script.js
+++ js/script.js
@@ -4,11 +4,17 @@ const imageWrapper = document.querySelector('.image-wrap');
 const btnUp = document.querySelector('.arrow-up');
 const btnDown = document.querySelector('.arrow-down');
 
-// 3. creare harray
-const listImage = [
+// 3. creare array
+const listImages = [
   'img/01.webp',
   'img/02.webp',
   'img/03.webp',
   'img/04.webp',
   'img/05.webp'
 ]
+
+//4. inserire img dentro html
+for(let i=0; i< listImages.length; i++){
+  const image = listImages[1];
+
+}
