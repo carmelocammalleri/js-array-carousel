cd88b3254a4167bdf3a25749e480f09513aa9885
Melozzo <carmecamma1995@gmail.com>
2023-09-13 15:51:59 +0200

    [edit]: btn in css and const in js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// 2. collegare elementi html/js
+
+const imageWrapper = document.querySelector('.image-wrap');
+const btnUp = document.querySelector('.arrow-up');
+const btnDown = document.querySelector('.arrow-down');
+
