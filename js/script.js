fbb8e4f41f050776e72d963d02921218a858de32
Melozzo <carmecamma1995@gmail.com>
2023-09-13 15:59:07 +0200

    [edit]: array list element

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ const imageWrapper = document.querySelector('.image-wrap');
 const btnUp = document.querySelector('.arrow-up');
 const btnDown = document.querySelector('.arrow-down');
 
+// 3. creare harray
+const listImage = [
+  'img/01.webp',
+  'img/02.webp',
+  'img/03.webp',
+  'img/04.webp',
+  'img/05.webp'
+]
